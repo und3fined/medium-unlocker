db6e15c0cb5652ab117c08b495a6dfbc18cb091b
und3fined <me@und3fined.com>
2022-01-01 13:57:52 +0700

    Hide medium banner

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
  | File Created: 31 Dec 2021 20:33:17
  | Author: und3fined (me@und3fined.com)
  | -----
- | Last Modified: 31 Dec 2021 21:31:07
+ | Last Modified: 01 Jan 2022 13:57:51
  | Modified By: und3fined (me@und3fined.com)
  | -----
  | Copyright (c) 2021 und3fined.com
@@ -16,7 +16,15 @@
 
   export let name;
 
+  const mediumMeterId = 'highlight-meter-';
+
   onMount(() => {
+    const mediumMeterElm = document.querySelector(`div[id*='${mediumMeterId}']`)
+
+    if (mediumMeterElm && mediumMeterElm.parentElement) {
+      mediumMeterElm.parentElement.style.display = 'none';
+    }
+
     const interval = setInterval(() => {
       if (!!hasElm(memberShipId) || !!hasElm(registerWall)) {
         window.location.reload();
